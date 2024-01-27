07044f079a7523eabea855753e2ea225ae165412
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2024-01-27 16:24:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const app = express();
+const listing = require("./models/listing.js");
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -16,6 +18,22 @@ async function main() {
 	await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+
+//Index Route
+app.get("/listings", async (req, res) => {
+	const allListings = await listing.find({});
+	res.render("./listings/index.ejs", { allListings });
+});
+
+// Show Route
+app.get("/listings/:id", async (req, res) => {
+	let { id } = req.params;
+	const listingData = await listing.findById(id);
+	res.render("./listings/show.ejs", { listingData });
+});
 app.get("/", (req, res) => {
 	res.send("Hi, I am root.");
 });
