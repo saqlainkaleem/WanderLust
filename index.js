8a2e07472c0f5fc67241b0e5be4a82be38155a5a
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2024-01-28 11:25:17 +0530

    Addded : Delete Route

--- index.js
+++ index.js
@@ -55,6 +55,12 @@ app.put("/listings/:id", async (req, res) => {
 	res.redirect(`/listings/${id}`);
 });
 
+app.delete("/listings/:id", async (req, res) => {
+	let { id } = req.params;
+	await listing.findByIdAndDelete(id);
+	res.redirect("/listings");
+});
+
 // Show Route
 app.get("/listings/:id", async (req, res) => {
 	let { id } = req.params;
