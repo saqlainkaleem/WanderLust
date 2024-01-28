43c07913bba0b5de1e6f4f178adfcb5cb2de8f35
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2024-01-28 11:18:31 +0530

    Added : Update Route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const ejs = require("ejs");
 const app = express();
 const listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -21,6 +22,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 //Index Route
 app.get("/listings", async (req, res) => {
@@ -28,7 +30,7 @@ app.get("/listings", async (req, res) => {
 	res.render("./listings/index.ejs", { allListings });
 });
 
-//New Route
+//Create Route
 app.get("/listings/new", (req, res) => {
 	res.render("./listings/new.ejs");
 });
@@ -37,6 +39,20 @@ app.post("/listings", async (req, res) => {
 	const newListing = new listing(req.body.listing);
 	await newListing.save();
 	res.redirect("/listings");
+});
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+	let { id } = req.params;
+	const listingData = await listing.findById(id);
+	res.render("./listings/edit.ejs", { listingData });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+	let { id } = req.params;
+	await listing.findByIdAndUpdate(id, { ...req.body.listing });
+	res.redirect(`/listings/${id}`);
 });
 
 // Show Route
