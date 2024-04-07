e2a72deed7f2d3ac89249534913c72eaababef5c
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2024-04-08 02:58:14 +0530

    Create ExpressError.js

---
+++ utils/ExpressError.js
@@ -0,0 +1,9 @@
+class ExpreeError extends Error {
+	constructor(statusCode, message) {
+		super();
+		this.statusCode = statusCode;
+		this.message = message;
+	}
+}
+
+module.exports = ExpreeError;
