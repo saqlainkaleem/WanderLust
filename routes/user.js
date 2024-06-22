f2975643ead88a74bfa5165963a67f1878a2ab10
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2024-06-22 12:32:39 +0530

    Added : User post route

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
-
+const User = require("../models/User.js");
+const wrapAsync = require("../utils/wrapasync.js");
 router.get("/signup", (req, res) => {
 	res.render("users/signup.ejs");
 });
+
+router.post(
+	"/signup",
+	wrapAsync(async (req, res) => {
+		try {
+			let { username, email, password } = req.body;
+			const newUser = new User({ email, username });
+			const registeredUser = await User.register(newUser, password);
+			console.log(registeredUser);
+			req.flash("success", "user registered successfully");
+			res.redirect("/listings");
+		} catch (e) {
+			req.flash("error", e.message);
+			res.redirect("/signup");
+		}
+	})
+);
 
 module.exports = router;
