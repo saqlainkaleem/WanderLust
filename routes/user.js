80df2134116d47b8c86bad6513a51d5b0550d501
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2024-06-23 14:31:21 +0530

    Added : post route for User

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User.js");
 const wrapAsync = require("../utils/wrapasync.js");
+const passport = require("passport");
 router.get("/signup", (req, res) => {
 	res.render("users/signup.ejs");
 });
@@ -26,4 +27,17 @@ router.post(
 router.get("/login", (req, res) => {
 	res.render("users/login.ejs");
 });
+
+router.post(
+	"/login",
+	passport.authenticate("local", {
+		failureRedirect: "/login",
+		failureFlash: true,
+	}),
+	async (req, res) => {
+		req.flash("success", "Welcome to Wanderlust!");
+		res.redirect("/listings");
+	}
+);
+
 module.exports = router;
