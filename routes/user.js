bfb4c7df694279a4d6d03277e86229a54af4193d
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2024-06-24 12:00:52 +0530

    Added : Auto login after signup for new user

--- routes/user.js
+++ routes/user.js
@@ -14,9 +14,13 @@ router.post(
 			let { username, email, password } = req.body;
 			const newUser = new User({ email, username });
 			const registeredUser = await User.register(newUser, password);
-			console.log(registeredUser);
-			req.flash("success", "user registered successfully");
-			res.redirect("/listings");
+			req.login(registeredUser, (err) => {
+				if (err) {
+					return next(err);
+				}
+				req.flash("success", "user registered successfully");
+				res.redirect("/listings");
+			});
 		} catch (e) {
 			req.flash("error", e.message);
 			res.redirect("/signup");
