533f56c334b9dd84951f46712cce3cdfdd49ec38
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2024-06-24 11:52:59 +0530

    Added : user options in navbar
    Login, Signup and logout options are added in the navbar

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ app.get("/", (req, res) => {
 app.use((req, res, next) => {
 	res.locals.success = req.flash("success");
 	res.locals.error = req.flash("error");
+	res.locals.currUser = req.user;
 	next();
 });
 
