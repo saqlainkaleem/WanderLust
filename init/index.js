0b01a1ecf4903cfa1c169ec4100b9c4bd0f02bcc
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2024-06-24 23:32:31 +0530

    Added : owner in initialization of DB

--- init/index.js
+++ init/index.js
@@ -17,6 +17,10 @@ async function main() {
 
 const initDB = async () => {
 	await Listing.deleteMany({});
+	initData.data = initData.data.map((obj) => ({
+		...obj,
+		owner: "66766b74fec457190f887839",
+	}));
 	await Listing.insertMany(initData.data);
 	console.log("Data was initialized");
 };
