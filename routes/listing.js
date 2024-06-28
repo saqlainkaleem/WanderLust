9477567010f85f13abfe614258681e9443571a60
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2024-06-29 01:15:33 +0530

    upload image to cloudinary

--- routes/listing.js
+++ routes/listing.js
@@ -2,14 +2,18 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapasync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+
+const upload = multer({ storage });
 
 router
 	.route("/")
 	.get(wrapAsync(listingController.index))
 	.post(
 		isLoggedIn,
+		upload.single("listing[image]"),
 		validateListing,
 		wrapAsync(listingController.createListing)
 	);
