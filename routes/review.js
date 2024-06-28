85533cb1727f0aeff3f4104eebfa084800b2d45e
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2024-06-28 18:53:33 +0530

    save author for review and user should be loggedIn

--- routes/review.js
+++ routes/review.js
@@ -3,16 +3,17 @@ const router = express.Router({ mergeParams: true });
 const listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapasync.js");
 const Review = require("../models/review.js");
-const { validateReview } = require("../middleware.js");
+const { validateReview, isLoggedIn } = require("../middleware.js");
 
 //Review Post Route
 router.post(
 	"/",
+	isLoggedIn,
 	validateReview,
 	wrapAsync(async (req, res) => {
 		let Listing = await listing.findById(req.params.id);
 		let newReview = new Review(req.body.review);
-
+		newReview.author = req.user._id;
 		Listing.reviews.push(newReview);
 
 		await newReview.save();
@@ -26,6 +27,7 @@ router.post(
 //Review Delete Route
 router.delete(
 	"/:reviewId",
+	isLoggedIn,
 	wrapAsync(async (req, res) => {
 		let { id, reviewId } = req.params;
 		await listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
