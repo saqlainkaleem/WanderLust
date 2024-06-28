5a8112eb78a4ff8b3107ceeef8458c0e2e40e517
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2024-06-28 17:47:24 +0530

    validateListing middleware

--- middleware.js
+++ middleware.js
@@ -27,3 +27,12 @@ module.exports.isOwner = async (req, res, next) => {
 	next();
 };
 
+module.exports.validateListing = (req, res, next) => {
+	const { error } = listingSchema.validate(req.body);
+	if (error) {
+		let errMsg = error.details.map((el) => el.message).join(",");
+		throw new ExpressError(400, errMsg);
+	} else {
+		next();
+	}
+};
