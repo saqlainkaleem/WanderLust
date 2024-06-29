d76182f049b45cb6675433732dd1a68aeb707d45
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2024-06-29 08:39:25 +0530

    original image preview while editing listing

--- controllers/listings.js
+++ controllers/listings.js
@@ -39,7 +39,11 @@ module.exports.editListingForm = async (req, res) => {
 		req.flash("error", "Listing you requested for does not exist!");
 		res.redirect("/listings");
 	}
-	res.render("./listings/edit.ejs", { listingData });
+
+	let originalImageUrl = listingData.image.url;
+	originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
+
+	res.render("./listings/edit.ejs", { listingData, originalImageUrl });
 };
 
 module.exports.updateListing = async (req, res) => {
